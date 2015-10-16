916dd4a928ac3b01ced11c16cc67cde59ce4788a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-17 01:53:25 +0200

    Adds database package tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,5 +9,9 @@
 
 node.default['rvm']['default_ruby'] = 'ruby-2.2.3'
 node.default['rvm']['user_default_ruby'] = 'ruby-2.2.3'
-
 include_recipe 'rvm::system'
+
+node.default['postgresql']['version'] = '9.3'
+node.default['postgresql']['password']['postgres'] = 'postgres'
+include_recipe 'postgresql::server'
+include_recipe 'postgresql::client'
--- test/integration/default/serverspec/database_spec.rb
+++ test/integration/default/serverspec/database_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe package('postgresql') do
+describe package('postgresql-9.3') do
+  it { should be_installed }
+end
+
+describe package('postgresql-client-9.3') do
   it { should be_installed }
 end

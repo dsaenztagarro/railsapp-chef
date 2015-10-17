178f6382b2b434ba1334e047ada1fa71e2f61a71
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-17 02:25:11 +0200

    Added test on database port

--- test/integration/default/serverspec/database_spec.rb
+++ test/integration/default/serverspec/database_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
-describe package('postgresql-9.3') do
-  it { should be_installed }
+describe 'PostgreSQL' do
+
+  it 'is listening on port 5432' do
+    expect(port(5432)).to be_listening
+  end
+
+  describe package('postgresql-9.3') do
+    it { should be_installed }
+  end
+
+  describe package('postgresql-client-9.3') do
+    it { should be_installed }
+  end
+
 end
 
-describe package('postgresql-client-9.3') do
-  it { should be_installed }
-end

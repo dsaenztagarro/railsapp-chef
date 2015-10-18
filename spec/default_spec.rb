baf418d765cd4b5e2d8a339b8408a5984382a724
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-18 02:15:09 +0200

    Fixed error require spec_helper

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,22 +1,6 @@
-require 'chefspec'
-require 'chefspec/berkshelf'
+require 'spec_helper'
 
 describe 'railsapp::default' do
-#   let (:chef_run) do
-#     Kernel.stub(:require).with('rvm')
-#     ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '12.04') do |node, server|
-#       node.set['postgresql']['version'] = '9.3'
-#     end.converge described_recipe
-#   end
-#
-#   before(:each) do
-#     stub_command('ls /var/lib/postgresql/9.3/main/recovery.conf').and_return(false)
-#   end
-#
-#   it 'converges successfully' do
-#     expect { :chef_run }.to_not raise_error
-#   end
-
 
   let (:chef_run) do
     Kernel.stub(:require).with('rvm')

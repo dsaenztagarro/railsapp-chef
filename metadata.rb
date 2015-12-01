16699ac96cbd154b14ff8ea425428faf96418f32
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-01 23:40:27 +0100

    Added metadata info

--- metadata.rb
+++ metadata.rb
@@ -10,3 +10,8 @@ depends 'build-essential'
 depends 'database_sl'
 depends 'rvm'
 depends 'phusionpassenger'
+
+%w(debian ubuntu).each { |os| supports os }
+
+source_url 'https://github.com/dsaenztagarro/railsapp' if respond_to?(:source_url)
+issues_url 'https://github.com/dsaenztagarro/railsapp/issues' if respond_to?(:issues_url)

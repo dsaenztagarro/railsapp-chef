dd1ba05e07f02546f6866a47d25602c53c01c891
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2016-01-03 15:34:13 +0100

    Refactoring to add server_name/server_alias to resources

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe 'railsapp::webserver' do
-  let(:server) { 'mywebapp.test' }
   let(:document_root) { '/var/www/mywebapp/public' }
+  let(:server_name) { 'mywebapp.test' }
+  let(:server_alias) { %w(mywebapp.test) }
+  let(:username) { 'deployer' }
+
   let(:chef_run) do
     ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04') do |node|
-      node.set['railsapp']['webserver']['name'] = server
+      node.set['railsapp']['webserver']['server_name'] = server_name
+      node.set['railsapp']['webserver']['server_alias'] = server_alias
       node.set['railsapp']['webserver']['document_root'] = document_root
+      node.set['railsapp']['username'] = username
     end.converge described_recipe
   end
 
@@ -17,15 +22,17 @@ describe 'railsapp::webserver' do
   it 'creates a new passenger site' do
     expect(chef_run).to(
       create_passenger_site('creating_site').with(
-        server: server,
-        document_root: document_root))
+        server_name: server_name,
+        server_alias: server_alias,
+        document_root: document_root,
+        user: username))
   end
 
   it 'enables a new passenger site' do
     expect(chef_run).to(
       enable_passenger_site('enabling_site').with(
-        server: server,
-        document_root: document_root))
+        server_name: server_name,
+        user: username))
   end
 
   it 'converges successfully' do

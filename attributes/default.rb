b973e2136c0c14f218bef3961faedb9756955f29
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2016-01-04 23:05:02 +0100

    Update dependencies. Fix keyserver host for rvm.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,4 @@ default['railsapp']['ruby_version'] = 'ruby-2.2.3'
 
 override['rvm']['user']['name'] = 'deployer'
 override['rvm']['user']['home'] = '/home/deployer'
+override['rvm']['keyserver'] = '213.133.103.71'

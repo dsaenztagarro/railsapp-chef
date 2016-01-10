2fb7041c9c2c0117a592e9220f95e2494f68108b
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2016-01-10 22:51:23 +0100

    Adds attribute examples

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,23 @@ default['rails_apps'] = []
 override['rvm']['user']['name'] = 'deployer'
 override['rvm']['user']['home'] = '/home/deployer'
 override['rvm']['keyserver'] = '213.133.103.71'
+
+# Examples:
+#
+# default['rails_apps'] = [
+#   {
+#     deploy_dir: '/var/www/myfamilyweb',
+#     rails_env: 'production',
+#     database: {
+#       adapter: 'postgresql',
+#       hostname: 'localhost',
+#       name: 'myfamilyweb',
+#       password: 'myfamilyweb',
+#       username: 'myfamilyweb'
+#     },
+#     secrets: {
+#       secret_key_base: 'abcdefghijklmnopqrstuvwxyz0123456789'
+#     }
+#   }
+# ]
+
